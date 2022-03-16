4424334c26ee31f6f7db3cf70d413a5ae9b77556
Recep Çelimli <98876147+celimlirecep@users.noreply.github.com>
2022-03-16 21:44:33 +0300

    update

--- Web_6_js/js_03_Anket/script.js
+++ Web_6_js/js_03_Anket/script.js
@@ -1,63 +1,80 @@
 const correctAnswers = ["E","E","E","E","E","E","E","E"];
 const form=document.querySelector("form");
 const result=document.querySelector(".result");
-const kullanıcıAd=document.querySelector("#ad");
-const kullanıcıSoyad=document.querySelector("#soyad");
+const kullaniciAd=form.querySelector("#ad");
+const bastakiResim=document.querySelector(".top-resim");
+const topRight=document.querySelector(".top-right");
+
+const kullaniciSoyad=document.querySelector("#soyad");
+const logo=document.querySelector(".logo-bolumu");
+const logokonumu=document.querySelector(".top-right")
+//console.log(logokonumu);
 let score=0;
+logo.addEventListener("click",logoyukle);
+
+function logoyukle(e)
+{
+    e.preventDefault();
+   if (e.target.classList.contains("logo-buton")) {
+       
+       bastakiResim.remove();
+       let html=e.target.children[0];
+       console.log(html);
+        const yeniresim=e.target.children[0];
+        topRight.appendChild(yeniresim);
+
+        console.log(topRight.children[0]);
+        
+       
+       
+       
+
+       
+   }
+}
 
 
 form.addEventListener("submit",e=>
 {
-    
     e.preventDefault();
 
-    
-   
     const userAnswer=[form.q1.value,form.q2.value,form.q3.value,form.q4.value,form.q5.value,form.q6.value,form.q7.value,form.q8.value];
     userAnswer.forEach((answer,index)=>
     {
         if (answer===correctAnswers[index]) score+=12.5;
     })
     result.classList.remove("result-none");
+    let score2=0;
+    const animateScore=setInterval(()=>{
+        
+        result.querySelector("span").textContent=`%${score2}`;
+        if (score2>=score) {
+            clearInterval(animateScore)
+            score=0;
+        }
+        else
+        {
+        if (score>=100) {
+            result.querySelector("p").textContent=`Tebrikler ${kullaniciAd.value+" "+kullaniciSoyad.value} köle olmaya hak kazandınız`;
+        }
+        else if(score>50){
+            result.querySelector("p").textContent="Tebrikler köle olmanıza az kaldı";
 
-    if (score==100) {
-     
-        result.querySelector("span").textContent=`%${score}`
-    }
-    else if(score>75){
-        result.textContent=`Scorunuz: ${score} , köle olmanıza az kaldı  `
-    }
-    else if(score>50){
-        result.textContent=`Scorunuz: ${score} , köle olma yolunda biraz daha çaba harcamalısınız `
-    }
-    else{
-        result.textContent=" BİZİM ŞİRKETİMİZE GİRMEYEREK NE KAYBETTİĞİNİN FARKINDAMISIN"
-    }
-    console.log(score);
-    
-let score2=0;
-console.log(score+"*************");
+        }
+        else
+        {
+            result.querySelector("p").textContent="Üzüldük köle olmadığığınız için kabul edemiyoruz";
 
-const animateScore=setInterval(()=>{
-    
-    result.querySelector("span").textContent=`%${score2}`;
-    if (score2>=score) {
-        clearInterval(animateScore)
-    }
-    else
-    {
-        score2++;
-    }
-    
-},20)
+        }
+            score2++;
+        }
+        
+    },20)
 
 })
-score=0;
+
 //*********************************SAAT */
 const clock=document.querySelector(".clock");
-
-
-
 function zamaniYaz() {
     const zaman=new Date();
 let saat=zaman.getHours();
--- Web_6_js/js_03_Anket/textboxdeneme/script.js
+++ Web_6_js/js_03_Anket/textboxdeneme/script.js
@@ -2,11 +2,12 @@ const form=document.querySelector("form");
 const mesaj=form.querySelector("#mesaj")
 const yaz=form.querySelector("p");
 
+
 form.addEventListener("submit",ekle);
 
 function ekle(e){
     e.preventDefault();
-    
+
     if (mesaj.value!=="") {
         yaz.innerText=mesaj.value;
     }
