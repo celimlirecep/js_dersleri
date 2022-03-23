de680bead1895eb37792bdc35740383e01bc9567
Recep Çelimli <98876147+celimlirecep@users.noreply.github.com>
2022-03-23 21:32:57 +0300

    update
    Karmaşık kavramlar

--- Bolum11/script.js
+++ Bolum11/script.js
@@ -26,11 +26,66 @@
 // const deger=kisi.soyle;
 // deger();
 
-const Araba=function(renk,model){
-    this.renk=renk;
-    this.model=model;
+// const Araba=function(renk,model){
+//     this.renk=renk;
+//     this.model=model;
+
+// }
+
+// const honda=new Araba("kırmızı","s7");
+// console.log(honda.model)
+
+//  const a={
+
+//     delete(){
+//         console.log(this)
+//         const lale= "lale";
+//         const kara=()=>{
+//             console.log(this)//fonksiyon içinde yazıldığında fonksiyonun sınıfını alıtr
+//             this.k();
+           
+//         }
+
+//         kara();
+//     },
+
+//     kerata: function(){
+
+//         console.log(this);
+//         this.k();
+//     },
+
+//     k:()=>{
+//         console.log("-------------------")
+//         console.log(this)
+//         console.log("--------------")
+//     }
+    
+
+
+//  }
+
+
+//  a.delete();
+//  a.kerata();
+//  a.k();
+
+
+const iller = {
+ illlerDizisi: ["istanbul","Ankara","Adıytaman","İstanbul"],
+ ad:"recep",
+
+// yaz(){
+//     this.illlerDizisi.forEach(function(element) {
+//         console.log(this);
+// });
+// }
+yaz(){
+    this.illlerDizisi.forEach(element=> {
+        console.log(this);
+});
+}
 
 }
 
-const honda=new Araba("kırmızı","s7");
-console.log(honda.model)
+iller.yaz();
